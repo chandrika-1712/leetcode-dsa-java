f3e78d8d1371755413044e3c1ca2ccf799fa00a1
chandrika-1712 <chandudangeti450@gmail.com>
2026-08-18 21:46:07 +0530

    Time: 1 ms (99.30%) | Memory: 44.3 MB (94.39%) - LeetSync

---
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -0,0 +1,47 @@
+/*
+ * Largest Almost Missing Integer
+ * Approach: Frequency Array + First/Last Element
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+
+        int[] freq = new int[51];
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int max = -1;
+        int n = nums.length;
+
+        if (k == 1) {
+            for (int i = 0; i <= 50; i++) {
+                if (freq[i] == 1) {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        if (k == n) {
+            for (int i = 0; i <= 50; i++) {
+                if (freq[i] > 0) {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        if (freq[nums[0]] == 1) {
+            max = Math.max(max, nums[0]);
+        }
+
+        if (freq[nums[n - 1]] == 1) {
+            max = Math.max(max, nums[n - 1]);
+        }
+
+        return max;
+    }
+}
