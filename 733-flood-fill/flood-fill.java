b40b85abc2e73d43cd753250f31db127a7cc338c
chandrika-1712 <chandudangeti450@gmail.com>
2026-09-06 20:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (98.43%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,36 +1,19 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m=image.length;
-        int n=image[0].length;
         int original=image[sr][sc];
         if(original==color)
          return image;
-        Queue<Pair> q=new LinkedList<>();
-        image[sr][sc]=color;
-        q.add(new Pair(sr,sc));
-          int[] delrow={-1,0,1,0};
-         int[] delcol={0,1,0,-1};
-        while(!q.isEmpty()){
-            Pair curr=q.poll();
-            int row=curr.row;
-            int col=curr.col;
-            for(int i=0;i<4;i++){
-               int dr=row+delrow[i];
-               int dc=col+delcol[i];
-               if(dr>=0&&dr<m&&dc>=0&&dc<n&&image[dr][dc]==original){
-                   q.add(new Pair(dr,dc));
-                   image[dr][dc]=color;
-               } 
-            }
-        }
+        dfs(image,original,color,sr,sc);
         return image;
+    }
+    void dfs(int[][] image,int original,int color,int sr,int sc){
+        if(sr<0||sr>=image.length||sc<0||sc>=image[0].length||
+          image[sr][sc]!=original)
+           return;
+        image[sr][sc]=color;
+        dfs(image,original,color,sr-1,sc);
+        dfs(image,original,color,sr,sc+1);
+        dfs(image,original,color,sr+1,sc);
+        dfs(image,original,color,sr,sc-1);
     }
 }
